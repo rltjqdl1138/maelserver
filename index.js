5193d08cbfdd2dbfd6da87622dc50a63a315e1d0
rltjqdl1138 <rltjqdl1138@naver.com>
2020-05-31 01:59:02 +0900

    add index

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const bodyParser = require('body-parser')
+const fs = require('fs');
+
+// Create Express app
+const app = express();
+const mainapp = require('./src/index')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/', mainapp)
+
+
+try{
+    //Load SSL Authentication Files
+    const passphrase = 'kks141592Az!'
+    const options = { 
+        ca: fs.readFileSync('./security/ssl/ca_bundle.crt'), 
+        pfx:  fs.readFileSync('./security/ssl/cert.pfx'), 
+        passphrase
+    };
+
+    // Create an HTTPS service.
+    https.createServer(options, app).listen(4001, ()=>
+        console.log('Mael Streaming Server on port \x1b[97m4001\x1b[0m with SSL Authentication')
+    );
+    // Create an HTTP service.
+    http.createServer(app).listen(4000,()=>
+        console.log('Mael Streaming Server on port \x1b[97m4000\x1b[31m without SSL Authentication\x1b[0m')
+    );
+
+}catch(e){
+    // Create an HTTP service.
+    http.createServer(app).listen(4000,()=>
+        console.log('Mael Streaming Server on port \x1b[97m4000\x1b[31m without SSL Authentication\x1b[0m')
+    );
+}
