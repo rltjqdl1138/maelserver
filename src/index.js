15b9d595d51c15283a88600de9dfbf0fed8381df
rltjqdl1138 <rltjqdl1138@naver.com>
2020-07-12 23:38:37 +0900

    today config

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ const APIRouter = require('./APIrouter')
 const Music = require('./Music')
 const Image = require('./Image')
 
-router.get('/',(req, res)=>{
-    res.json({msg:'welcom mael'})
-})
+router.get('/',(req, res)=>res.json({msg:'welcom mael'}))
 
 router.use('/api', APIRouter )
 router.use('/music', Music)
 router.use('/image', Image)
-
-
-//const { MessageService } = require('./Services/naverCloud')
-//const messageService = new MessageService()
-//const a = await messageService.sendMessage('82','01066626386')
-
 
 module.exports = router
